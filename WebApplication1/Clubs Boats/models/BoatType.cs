81c63f1c40b522fbd1b32be630bd29fae6304665
Emil28 <65360960+Emil28@users.noreply.github.com>
2025-12-07 14:56:26 +0100

    Reapply "emil"
    This reverts commit b1e92a550b804c98cee70d0a42785574d3441aa1.

---
+++ WebApplication1/Clubs Boats/models/BoatType.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Clubs_Boats.models
+{
+    public class BoatType
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ WebApplication1/Clubs Boats/models/Maintenance.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Clubs_Boats.models
+{
+    public class Maintenance
+    {
+        public DateTime Date { get; set; }
+        public MaintenanceType Type { get; set; }
+        public string Description { get; set; }
+
+    }
+}
